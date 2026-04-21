f8edcaf3c2c765cf49cb25dcf38c25a401dc9549
KaltrinaMurati <rfazliu53@gmail.com>
2026-04-21 20:29:40 +0200

    u shtua nje komponent

---
+++ components/test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {View,Text} from "react-native";
+
+export default function Test(){
+    return(
+        <View>
+            <Text>Ky eshte nje shembull i komponentit Text</Text>
+        </View>
+    )
+}
